2e1eb7242ba1baa72b3b969d2232dc19e2b49768
lathan britz <lathan.britz@drupalconnect.com>
2022-11-19 19:56:42 -0300

    err

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,20 @@ import vue from '@vitejs/plugin-vue'
 
 import fs from 'fs'
 
-// export default defineConfig({
-//   plugins: [vue()],
-//   server: {
-//     https: {
-//       key: fs.readFileSync('../../ssl/panicbot.xyz.key'),
-//       cert: fs.readFileSync('../../ssl/panicbot_xyz.crt'),
-//     },
-//   },
-//   define: {
-//     'process.env': {}
-//   }
-// })
-
 export default defineConfig({
-  plugins: [vue()] 
+  plugins: [vue()],
+  server: {
+    https: {
+      key: fs.readFileSync('../../ssl/panicbot.xyz.key'),
+      cert: fs.readFileSync('../../ssl/panicbot_xyz.crt'),
+    },
+  },
+  define: {
+    'process.env': {}
+  }
 })
+
+// export default defineConfig({
+//   plugins: [vue()] 
+// })
 
