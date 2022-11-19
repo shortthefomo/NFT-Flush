b5aa751e4791dcb543aeeab08012bb91937b3e7b
lathan britz <lathan.britz@drupalconnect.com>
2022-11-19 10:58:20 -0300

    debug

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -115,6 +115,8 @@
                 }
                 const res = await this.client.send(payload)
                 this.NFTokenOffers = []
+                console.log('objects', res.account_objects)
+                
                 for (let index = 0; index < res.account_objects.length; index++) {
                     const element = res.account_objects[index]
                     if (element?.LedgerEntryType === 'NFTokenOffer') {
