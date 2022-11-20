1fa896ef32b1f3a2fdef5704ccedda7f4c766571
lathan britz <lathan.britz@drupalconnect.com>
2022-11-19 21:42:43 -0300

    drop type button

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -7,8 +7,8 @@
             </p>
             <p class="col-md-12 fs-4">
                 <p class="text-center">
-                    <a v-if="account != '' && hasOffers" type="button" class="btn btn-purple me-2" @click="flushAll" role="button" id="flushAll">flush all</a>
-                    <a v-if="account != '' && hasSelected" type="button" class="btn btn-pink me-2" @click="flushSelected" role="button" id="flushSelected">flush selected</a>
+                    <a v-if="account != '' && hasOffers" class="btn btn-purple me-2" @click="flushAll" role="button" id="flushAll">flush all</a>
+                    <a v-if="account != '' && hasSelected" class="btn btn-pink me-2" @click="flushSelected" role="button" id="flushSelected">flush selected</a>
                 </p>
             </p>
         </div>
