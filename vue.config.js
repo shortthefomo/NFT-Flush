7f27d0920c40f051bc4ad6ff8b04157f894258db
lathan britz <lathan.britz@drupalconnect.com>
2022-12-03 11:21:27 -0300

    do this

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
+import fs from 'fs'
+
 module.exports = {
     // options...
     devServer: {
-        allowedHosts: 'all'
+        allowedHosts: 'all',
+        https: {
+            key: fs.readFileSync('../../ssl/panicbot.xyz.key'),
+            cert: fs.readFileSync('../../ssl/panicbot_xyz.crt'),
+        },
     }
 }
