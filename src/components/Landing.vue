262488b991c5e92e2932114973c70434d0092d7b
lathan britz <lathan.britz@drupalconnect.com>
2022-12-09 23:30:43 -0300

    buy/sell buttons

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -25,8 +25,7 @@
                 </thead>
                 <tbody>
                     <tr v-for="row in NFTokenOffers" @click="selectedRow(row)" :class="highlights(row)">
-                        <td v-if="typeof row['Amount'] === 'object'">{{numeralFormat((row['Amount'].value/1_000_000), '0,0[.]00000000') }} {{currencyHexToUTF8(row['Amount'].currency)}}</td>
-                        <td v-else>{{numeralFormat((row['Amount']/1_000_000), '0,0[.]00000000')}} XRP</td>
+                        <td>{{row['Flags'] == 1 ? 'sell':'buy'}}</td>
                         <td style="max-width:60px;" v-if="('Image' in row)"><img class="img-fluid" :src="row['Image']" :alt="row['OfferID']" :title="row['OfferID']"></td>
                         <td v-else style="width:60px !important; overflow: hidden;">...{{row['OfferID'].substring(row['OfferID'].length-5, row['OfferID'].length)}}</td>
                     </tr>
@@ -88,7 +87,7 @@
                 if (this.NFTokenOffers.length == 0) {
                     return []
                 }
-                return ['Offered', 'NFT']
+                return ['Side', 'NFT']
                 //return Object.keys(this.NFTokenOffers[0]).filter( code => code !== 'ledger')
             }
         },
