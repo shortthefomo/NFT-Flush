e44631bc8988698719daa32a40283afe325f4024
lathan britz <lathan.britz@drupalconnect.com>
2022-12-24 14:01:34 -0300

    dev viewer

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -1,105 +1,107 @@
 <template>
-    <div class="container-fluid py-5">
-        <div class="media">
-            <img v-if="'image' in account_nfts[selected]['data']" :src="account_nfts[selected]['data']['image']" class="img-fluid"/>
-            <video v-else-if="'video' in account_nfts[selected]['data']" class="img-fluid">
-                <source :src="account_nfts[selected]['data']['video']" :type="'video/' + account_nfts[selected]['data']['video_extension']">
-            </video>
-        </div>
+    <div class="media">
+        <div v-if="'image' in account_nfts[selected]['data']" 
+                :style="'background-repeat: no-repeat;background-position: center;background-size: cover;height: 100%;background-image: url('
+                    + account_nfts[selected]['data']['image'] + '");'"></div>
+        <video v-else-if="'video' in account_nfts[selected]['data']" class="img-fluid">
+            <source :src="account_nfts[selected]['data']['video']"
+                :type="'video/' + account_nfts[selected]['data']['video_extension']">
+        </video>
     </div>
 </template>
 
 <script>
-    const xapp = window.xAppSdk
-    import { Buffer } from 'buffer'
+const xapp = window.xAppSdk
+import { Buffer } from 'buffer'
 
-    export default {
-        name: 'Viewer',
-        props: ['client', 'Sdk', 'nodetype'],
-        data() {
-            return {
-                selected: 0,
-                account_nfts: [],
-                three: null,
-                image: null,
-                video: null,
-                audio: null
+export default {
+    name: 'Viewer',
+    props: ['client', 'Sdk', 'nodetype'],
+    data() {
+        return {
+            selected: 0,
+            account_nfts: [],
+            three: null,
+            image: null,
+            video: null,
+            audio: null
+        }
+    },
+    async mounted() {
+        console.log('viewer mounted...')
+        await this.fetchNFTs()
+    },
+    methods: {
+        async fetchNFTs() {
+            const payload = {
+                'id': 9,
+                'command': 'account_nfts',
+                'account': this.$store.getters.getAccount,
+                'ledger_index': 'validated'
+            }
+
+            let res = await this.client.send(payload)
+            this.account_nfts = res.account_nfts
+            while (res['marker'] !== undefined) {
+                console.log('marker', res['marker'])
+                payload.marker = res['marker']
+                const res = await this.client.send(payload)
+                this.account_nfts.concat(res.account_nfts)
+            }
+            console.log('account_nfts', this.account_nfts)
+            for (let index = 0; index < this.account_nfts.length; index++) {
+                const element = this.account_nfts[index]
+                this.getImageURL(element, index)
             }
         },
-        async mounted() {
-            console.log('viewer mounted...')
-            await this.fetchNFTs()
-        },
-        methods: {
-            async fetchNFTs() {
-                const payload = {
-                    'id': 9,
-                    'command': 'account_nfts',
-                    'account': this.$store.getters.getAccount,
-                    'ledger_index': 'validated'
-                }
-
-                let res = await this.client.send(payload)
-                this.account_nfts = res.account_nfts           
-                while (res['marker'] !== undefined) {
-                    console.log('marker', res['marker'])
-                    payload.marker = res['marker']
-                    const res = await this.client.send(payload)
-                    this.account_nfts.concat(res.account_nfts)
-                }
-                console.log('account_nfts', this.account_nfts)
-                for (let index = 0; index < this.account_nfts.length; index++) {
-                    const element = this.account_nfts[index]
-                    this.getImageURL(element, index)
-                }
-            },
-            async getImageURL(NFT,index) {
-                try {
-                    const URI = Buffer.from(NFT.URI, 'hex').toString('utf8')
-                    await this.convertURI(URI, index)
-                    return true
-                } catch (e) {
-                    console.log('error', e)
-                }
-                console.log('NOT FOUND!!!')
-                return false
-            },
-            async convertURI(URI, index) {
-                const convertedURI = URI.replace('ipfs://', 'https://ipfs.io/ipfs/')
-                const {data} = await this.axios.get(convertedURI, { timeout: 1000 })
-                this.account_nfts[index]['data'] = {}
-                console.log('data', data)
-                if ('image' in data) {
-                    const result = data.image.replace('ipfs://', 'https://ipfs.io/ipfs/')
-                    if (result != '') {
-                        this.account_nfts[index]['data']['image'] = result
-                        console.log('image', result)
-                    }
-                }
-
-                if ('video' in data) {
-                    const result = data.video.replace('ipfs://', 'https://ipfs.io/ipfs/')
-                    if (result != '') {
-                        this.account_nfts[index]['data']['video'] = result
-                        console.log('video', result)
-                        this.account_nfts[index]['data']['video_extension'] = this.getExtensionURL()
-                    }
-                }
-
-                if ('audio' in data) {
-                    const result = data.audio.replace('ipfs://', 'https://ipfs.io/ipfs/')
-                    if (result != '') {
-                        this.account_nfts[index]['data']['audio'] = result
-                        console.log('audio', result)
-                    }
-                }
-            },
-            getExtensionURL(url) {
-                return url.split(/[#?]/)[0].split('.').pop().trim();
+        async getImageURL(NFT, index) {
+            try {
+                const URI = Buffer.from(NFT.URI, 'hex').toString('utf8')
+                await this.convertURI(URI, index)
+                return true
+            } catch (e) {
+                console.log('error', e)
             }
+            console.log('NOT FOUND!!!')
+            return false
+        },
+        async convertURI(URI, index) {
+            const convertedURI = URI.replace('ipfs://', 'https://ipfs.io/ipfs/')
+            const { data } = await this.axios.get(convertedURI, { timeout: 1000 })
+            this.account_nfts[index]['data'] = {}
+            console.log('data', data)
+            if ('image' in data) {
+                const result = data.image.replace('ipfs://', 'https://ipfs.io/ipfs/')
+                if (result != '') {
+                    this.account_nfts[index]['data']['image'] = result
+                    console.log('image', result)
+                }
+            }
+
+            if ('video' in data) {
+                const result = data.video.replace('ipfs://', 'https://ipfs.io/ipfs/')
+                if (result != '') {
+                    this.account_nfts[index]['data']['video'] = result
+                    console.log('video', result)
+                    this.account_nfts[index]['data']['video_extension'] = this.getExtensionURL()
+                }
+            }
+
+            if ('audio' in data) {
+                const result = data.audio.replace('ipfs://', 'https://ipfs.io/ipfs/')
+                if (result != '') {
+                    this.account_nfts[index]['data']['audio'] = result
+                    console.log('audio', result)
+                }
+            }
+        },
+        getExtensionURL(url) {
+            return url.split(/[#?]/)[0].split('.').pop().trim();
         }
     }
+}
 </script>
 
 <style scoped>
+
 </style>
