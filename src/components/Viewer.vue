f47f17bedfafc106bf49a8771eae58c0de52045a
lathan britz <lathan.britz@drupalconnect.com>
2022-12-24 15:35:33 -0300

    dev viewer

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -8,8 +8,8 @@
                 :type="'video/' + account_nfts[selected]['data']['video_extension']">
         </video> -->
         <video class="w-100" :autoplay="autoplay" :loop="loop" :muted="muted" :poster="account_nfts[selected]['data']['image']">
-            <source src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4#t=9"
-                type="video/mp4">
+            <!-- <source src="https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4#t=9"
+                type="video/mp4"> -->
         </video>
     </div>
 </template>
