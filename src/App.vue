4e2bb533802a78dc673add808e319ee0a23eb199
lathan britz <lathan.britz@drupalconnect.com>
2022-12-24 13:55:09 -0300

    dev viewer

--- src/App.vue
+++ src/App.vue
@@ -8,12 +8,12 @@
     <main class="container flex-shrink-0 mb-4">
     </main>
 
-    <footer class="container footer mb-2 mt-auto">
+    <!-- <footer class="container footer mb-2 mt-auto">
         <div class="border-top py-3">
             &copy; Three
             <Refs />
         </div>
-    </footer>
+    </footer> -->
 </template>
 
 <script>
