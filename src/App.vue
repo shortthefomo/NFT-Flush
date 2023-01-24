803430d4511c17b01f8482df64b65cc6a6285376
lathan britz <lathan.britz@drupalconnect.com>
2023-01-24 14:19:11 -0300

    now set and get

--- src/App.vue
+++ src/App.vue
@@ -64,13 +64,12 @@
             async getStoreage() {
                 console.log('getStoreage URL', `${this.connection.url}/api/v1/apps/app-storage?account=${this.$store.getters.getAccount}&appkey=${import.meta.env.VITE_APP_NFT_KEY}`)
 
-
                 const headers = { 'Content-Type': 'application/json; charset=utf-8' }
                 const payload = await this.axios.post(`${this.connection.url}/api/v1/apps/app-storage?account=${this.$store.getters.getAccount}&appkey=${import.meta.env.VITE_APP_NFT_KEY}`, JSON.stringify({'json': 'somethign fun'}), { headers })
                 console.log('payload', payload)
 
 			    const {data} = await this.axios.get(`${this.connection.url}/api/v1/apps/app-storage?account=${this.$store.getters.getAccount}&appkey=${import.meta.env.VITE_APP_NFT_KEY}`, { timeout: 1000 })
-			    console.log('getStorage', data)
+			    console.log('getStorage',  Buffer.from(data.store, 'hex').toString('utf8'))
             },
             async jwtFlow() {
                 const tokenData = await this.Sdk.getOttData()
